2053fc9a1a012f110b5c120b6f90dc63150c6880
Pero Knezevic <djpero.84@gmail.com>
2015-07-12 22:32:31 +0200

    Some design fix Add design header/footer and logo Linking font

--- TooPixelContact/resources/views/app.blade.php
+++ TooPixelContact/resources/views/app.blade.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>TooPixel Contact Page</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+    <link href="//fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css">
 </head>
 <body>
+    <header>
+        <div>
+            <a href="/" ><img src="{{ URL::asset('logo_en.png') }}" alt="logo"/></a>
+        </div>
+
+    </header>
     <div class="content">
         @yield('content')
     </div>
+    <footer>
+        Pero
+    </footer>
 </body>
 </html>
