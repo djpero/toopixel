8f2e30608070ed8ae3216fe1c21858d9a2f827d2
Pero Knezevic <djpero.84@gmail.com>
2015-07-13 00:02:52 +0200

    French localization added for labels and text-s

---
+++ TooPixelContact/resources/lang/fr/localization.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'name' => 'Nom',
+    'surname' => 'Nom de famille',
+    'email' => 'Email',
+    'message' => 'Message',
+    'image' => 'Image',
+    'send' => 'ENVOYER',
+    'contact_us' => 'Contactez Nous',
+    'contact_success' => 'Votre demande de contact est envoyé avec succès',
+    'will_contact_you' => 'nous vous contacterons rapidement',
+);
+
